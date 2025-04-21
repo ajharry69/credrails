0540d236b6c6ddae0f7fb5d45b5b906b2da86ef5
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-04-21 12:09:47 +0300

    Use `namedtuple` for better readability

--- credrails/apps/reconciliation/serializers.py
+++ credrails/apps/reconciliation/serializers.py
@@ -1,12 +1,16 @@
 import codecs
 import csv
 import re
+from collections import namedtuple
 
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from credrails.apps.reconciliation.utils import CellId
 from credrails.apps.reconciliation.validators import FileMimeTypeValidator
+
+ValidData = namedtuple("ValidData", ["data", "column_positions"])
+RecordValue = namedtuple("RecordValue", ["row_number", "data"])
 
 
 class ReconciliationSerializer(serializers.Serializer):
@@ -24,13 +28,13 @@ class ReconciliationSerializer(serializers.Serializer):
                 raise ValidationError(detail="Invalid CSV data") from e
 
             record_id = reader.fieldnames[0]
-            return {
-                "column_positions": {col_name: position for position, col_name in enumerate(reader.fieldnames, start=1)},
-                "data": {
-                    row[record_id.lower()]: (index, row)
+            return ValidData(
+                data={
+                    row[record_id.lower()]: RecordValue(row_number=index, data=row)
                     for index, row in enumerate(reader, start=2)  # starting at 2 because 1 is headers
                 },
-            }
+                column_positions={col_name: position for position, col_name in enumerate(reader.fieldnames, start=1)},
+            )
 
     def validate_target(self, value):
         return self.validate_source(value=value)
@@ -38,53 +42,43 @@ class ReconciliationSerializer(serializers.Serializer):
     def to_representation(self, instance):
         discrepancies = []
         records_missing_in_source = []
-        records_missing_in_target = []
 
-        source = instance["source"]
-        source_data, source_column_positions = source["data"], source["column_positions"]
-        target = instance["target"]
-        target_data, _ = target["data"], target["column_positions"]
+        source: ValidData = instance["source"]
+        target: ValidData = instance["target"]
 
-        for key, value in source_data.items():
-            if key not in target_data:
-                row_number, data = value
-                records_missing_in_target.append(data)
-
-        for key, value in target_data.items():
-            row_number, data = value
-            source_value = source_data.get(key)
+        for key, value in target.data.items():
+            source_value: RecordValue = source.data.get(key)
             if source_value is None:
-                records_missing_in_source.append(data)
+                records_missing_in_source.append(value.data)
                 continue
 
-            target_source_row_number, target_source_data = source_value
-            for column_name, column_value in data.items():
-                if target_source_data[column_name] == column_value:
+            for column_name, column_value in value.data.items():
+                if source_value.data[column_name] == column_value:
                     continue
 
                 reason = "Mismatching date format"  # assume we are looking at a date as the last resort
 
-                if target_source_data[column_name].lower() == column_value.lower():
+                if source_value.data[column_name].lower() == column_value.lower():
                     reason = "Mismatching case"
                 elif re.match(r"^\d+(\.\d+)?$", column_value):
-                    if float(target_source_data[column_name]) == float(column_value):
+                    if float(source_value.data[column_name]) == float(column_value):
                         continue
 
                     reason = "Mismatching numbers"
 
                 cell_id = CellId(
-                    row_number=target_source_row_number,
-                    column_number=source_column_positions[column_name],
+                    row_number=source_value.row_number,
+                    column_number=source.column_positions[column_name],
                 )
                 discrepancy = {
                     "spreadsheet_cell_id": str(cell_id),
                     "column_name": column_name,
-                    "row_number": target_source_row_number,
+                    "row_number": source_value.row_number,
                     "reason": reason,
                 }
                 discrepancies.append(discrepancy)
 
-        instance["records_missing_in_source"] = records_missing_in_source
-        instance["records_missing_in_target"] = records_missing_in_target
         instance["discrepancies"] = discrepancies
+        instance["records_missing_in_source"] = records_missing_in_source
+        instance["records_missing_in_target"] = [v.data for k, v in source.data.items() if k not in target.data]
         return super().to_representation(instance)
